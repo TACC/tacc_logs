dc6c903585eaf8021e424f06739afbc536e9543a
Todd Evans <rtevans@localhost.localdomain>
2015-02-12 20:03:45 -0600

    updated formatting for log output

--- rational_printk_module/rational_printk.c
+++ rational_printk_module/rational_printk.c
@@ -1,11 +1,13 @@
 #include <linux/module.h>
 #include <linux/kprobes.h>
 #include <linux/ctype.h>
-#define LOG_LINE_MAX     1024-32
+#define LOG_LINE_MAX     1024
 
 static int rpk_encode2(char *buf, size_t size, 
-			  const char *fmt, va_list args);
+		       const char *fmt, va_list args);
 
+static char *rpk_encode_args(char *pos, char *end, int *key, const char *fmt,
+			     va_list args);
 
 static asmlinkage int ins_printk(const char *fmt, ...) 
 {
@@ -16,7 +18,7 @@ static asmlinkage int ins_printk(const char *fmt, ...)
 
   va_start(args,fmt);
   r =  rpk_encode2(msg, sizeof(msg), fmt, args);
-  printk("%s",msg);
+  printk(KERN_INFO "%s",msg);
   va_end(args);
 
   jprobe_return();
@@ -74,7 +76,7 @@ static inline char *rpk_str(char *pos, char *end, const char *str, int prec)
   for (; prec > 0 && *str != 0; prec--, str++) {
     if (*str == '\n') continue;
 
-  #ifdef KERN_SOH_ASCII
+#ifdef KERN_SOH_ASCII
     if (*str != KERN_SOH_ASCII) 
       rpk_put(pos, end, *str);
     else {
@@ -84,9 +86,9 @@ static inline char *rpk_str(char *pos, char *end, const char *str, int prec)
       rpk_put(pos, end, '>');
       return pos;
     }
-  #else
+#else
     rpk_put(pos, end, *str);
-  #endif
+#endif
   }
   return pos;
 }
@@ -95,210 +97,222 @@ static inline char *rpk_str(char *pos, char *end, const char *str, int prec)
 static char *rpk_encode_num(char *pos, char *end, int *key,
 			    unsigned long long num, int sign)
 {
-	pos = rpk_key(pos, end, key);
-	rpk_put(pos, end, ':');
-	rpk_put(pos, end, ' ');
-	pos = rpk_num(pos, end, num, sign);
-	rpk_put(pos, end, '\n');
+  pos = rpk_key(pos, end, key);
+  rpk_put(pos, end, ':');
+  rpk_put(pos, end, ' ');
+  pos = rpk_num(pos, end, num, sign);
+  rpk_put(pos, end, '\n');
 
-	return pos;
+  return pos;
 }
 
 /* Encode string argument. Format is "<key>:<upto-prec-chars-from-string>\0" */
 static char *rpk_encode_str(char *pos, char *end, int *key, const char *str,
 			    int prec)
 {
-	pos = rpk_key(pos, end, key);
-	rpk_put(pos, end, ':');
-	rpk_put(pos, end, ' ');
-	pos = rpk_str(pos, end, str, prec);
-	rpk_put(pos, end, '\n');
+  pos = rpk_key(pos, end, key);
+  rpk_put(pos, end, ':');
+  rpk_put(pos, end, ' ');
+  pos = rpk_str(pos, end, str, prec);
+  rpk_put(pos, end, '\n');
 
-	return pos;
+  return pos;
 }
 
 static char *rpk_encode_args(char *pos, char *end, int *key, const char *fmt,
 			     va_list args)
 {
-	int prec, qual, sign;
-	unsigned long long num;
-	const char *str;
+  int prec, qual, sign;
+  unsigned long long num;
+  const char *str;
+  va_list va;
+  void *ptr;
 
-	for (; *fmt != 0; ++fmt) {
-		if (*fmt != '%')
-			continue;
-		/* Ignore all flags. */
-	      repeat:
-		++fmt;		/* This also skips the '%'. */
-		switch (*fmt) {
-		case '-':
-			goto repeat;
-		case '+':
-			goto repeat;
-		case ' ':
-			goto repeat;
-		case '#':
-			goto repeat;
-		case '0':
-			goto repeat;
-		}
+  for (; *fmt != 0; ++fmt) {
+    if (*fmt != '%')
+      continue;
+    /* Ignore all flags. */
+  repeat:
+    ++fmt;		/* This also skips the '%'. */
+    switch (*fmt) {
+    case '-':
+      goto repeat;
+    case '+':
+      goto repeat;
+    case ' ':
+      goto repeat;
+    case '#':
+      goto repeat;
+    case '0':
+      goto repeat;
+    }
 
-		/* Field width. */
-		if (isdigit(*fmt)) {	/* Ignore embedded field width. */
-			(void)rpk_skip_atoi(&fmt);
-		} else if (*fmt == '*') {	/* Specified by next argument. */
-			++fmt;
-			pos =
-			    rpk_encode_num(pos, end, key, va_arg(args, int), 1);
-		}
+    /* Field width. */
+    if (isdigit(*fmt)) {	/* Ignore embedded field width. */
+      (void)rpk_skip_atoi(&fmt);
+    } else if (*fmt == '*') {	/* Specified by next argument. */
+      ++fmt;
+      pos =
+	rpk_encode_num(pos, end, key, va_arg(args, int), 1);
+    }
 
-		/* Precision. */
-		prec = INT_MAX;
-		if (*fmt == '.') {
-			++fmt;
-			if (isdigit(*fmt)) {
-				prec = rpk_skip_atoi(&fmt);
-			} else if (*fmt == '*') {	/* Specified by next argument. */
-				++fmt;
-				prec = va_arg(args, int);
-				pos = rpk_encode_num(pos, end, key, prec, 1);
-			}
-			if (prec < 0)
-				prec = 0;
-		}
+    /* Precision. */
+    prec = INT_MAX;
+    if (*fmt == '.') {
+      ++fmt;
+      if (isdigit(*fmt)) {
+	prec = rpk_skip_atoi(&fmt);
+      } else if (*fmt == '*') {	/* Specified by next argument. */
+	++fmt;
+	prec = va_arg(args, int);
+	pos = rpk_encode_num(pos, end, key, prec, 1);
+      }
+      if (prec < 0)
+	prec = 0;
+    }
 
-		/* Conversion qualifier. */
-		qual = 0;
-		if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L' ||
-		    *fmt == 'Z' || *fmt == 'z' || *fmt == 't') {
-			qual = *fmt;
-			++fmt;
+    /* Conversion qualifier. */
+    qual = 0;
+    if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L' ||
+	*fmt == 'Z' || *fmt == 'z' || *fmt == 't') {
+      qual = *fmt;
+      ++fmt;
 
-			if (qual == 'h' && *fmt == 'h') {
-				qual = 'H';
-				++fmt;
-			}
+      if (qual == 'h' && *fmt == 'h') {
+	qual = 'H';
+	++fmt;
+      }
 
-			if (qual == 'l' && *fmt == 'l') {
-				qual = 'L';
-				++fmt;
-			}
-		}
+      if (qual == 'l' && *fmt == 'l') {
+	qual = 'L';
+	++fmt;
+      }
+    }
 
-		sign = 0;
+    sign = 0;
 
-		switch (*fmt) {
-		case 'c':
-			pos =
-			    rpk_encode_num(pos, end, key, va_arg(args, int), 1);
-			continue;
+    switch (*fmt) {
+    case 'c':
+      pos =
+	rpk_encode_num(pos, end, key, va_arg(args, int), 1);
+      continue;
 
-		case 's':
-			str = va_arg(args, char *);
-			if ((unsigned long)str < PAGE_SIZE)
-				str = "<NULL>";
-			pos = rpk_encode_str(pos, end, key, str, prec);
-			continue;
+    case 's':
+      str = va_arg(args, char *);
+      if ((unsigned long)str < PAGE_SIZE)
+	str = "<NULL>";
+      pos = rpk_encode_str(pos, end, key, str, prec);
+      continue;
 
-		case 'p':
-			pos = rpk_encode_num(pos, end, key,
-					     (unsigned long)va_arg(args,
-								   void *), 0);
-			continue;
+    case 'p':
+      if (*(fmt+1) == 'V') {	
+	ptr = va_arg(args, void*);
+	va_copy(va, *((struct va_format *)ptr)->va);
+	pos = rpk_encode_str(pos, end, key, 
+			     ((struct va_format *)ptr)->fmt, prec);
+	pos = rpk_encode_args(pos, end, key, 
+			      ((struct va_format *)ptr)->fmt, va);
+	continue;
+      }      
+      else
+	pos = rpk_encode_num(pos, end, key,
+			     (unsigned long)va_arg(args,
+						   void *), 0);
+      continue;
 
-		case 'n':	/* Ignore. */
-			(void)va_arg(args, void *);
-			continue;
+    case 'n':	/* Ignore. */
+      (void)va_arg(args, void *);
+      continue;
 
-		case '%':
-			continue;
+    case '%':
+      continue;
 
-			/* Integer number formats. */
-		case 'd':
-		case 'i':
-			sign = 1;
-		case 'o':
-		case 'u':
-		case 'x':
-		case 'X':
-			if (qual == 'L') {
-				num = va_arg(args, long long);
-			} else if (qual == 'l') {
-				num = va_arg(args, unsigned long);
-				if (sign)
-					num = (signed long)num;
-			} else if (qual == 'Z' || qual == 'z') {
-				num = va_arg(args, size_t);
-			} else if (qual == 't') {
-				num = va_arg(args, ptrdiff_t);
-			} else if (qual == 'h') {
-				num = (unsigned short)va_arg(args, int);
-				if (sign)
-					num = (signed short)num;
-			} else if (qual == 'H') {
-				num = (unsigned char)va_arg(args, int);
-				if (sign)
-					num = (signed char)num;
-			} else {
-				num = va_arg(args, unsigned int);
-				if (sign)
-					num = (signed int)num;
-			}
+      /* Integer number formats. */
+    case 'd':
+    case 'i':
+      sign = 1;
+    case 'o':
+    case 'u':
+    case 'x':
+    case 'X':
+      if (qual == 'L') {
+	num = va_arg(args, long long);
+      } else if (qual == 'l') {
+	num = va_arg(args, unsigned long);
+	if (sign)
+	  num = (signed long)num;
+      } else if (qual == 'Z' || qual == 'z') {
+	num = va_arg(args, size_t);
+      } else if (qual == 't') {
+	num = va_arg(args, ptrdiff_t);
+      } else if (qual == 'h') {
+	num = (unsigned short)va_arg(args, int);
+	if (sign)
+	  num = (signed short)num;
+      } else if (qual == 'H') {
+	num = (unsigned char)va_arg(args, int);
+	if (sign)
+	  num = (signed char)num;
+      } else {
+	num = va_arg(args, unsigned int);
+	if (sign)
+	  num = (signed int)num;
+      }
 
-			pos = rpk_encode_num(pos, end, key, num, sign);
-			continue;
+    pos = rpk_encode_num(pos, end, key, num, sign);
+    continue;
 
-		default:	/* This is what the kernel's vsprintf does. */
-			if (*fmt == 0)
-				goto out;
-			continue;
-		}
-	}
-      out:
-	return pos;
+    default:	/* This is what the kernel's vsprintf does. */
+      if (*fmt == 0)
+	goto out;
+      continue;
+    }
+  }
+ out:
+  return pos;
 }
 
 static inline int rpk_get_level(const char *buffer)
 {
 #ifdef KERN_SOH_ASCII
-	if (buffer[0] == KERN_SOH_ASCII && buffer[1]) {
-		switch (buffer[1]) {
-		case '0' ... '7':
-		case 'd':	/* KERN_DEFAULT */
-			return buffer[1];
-		}
-	}
+  if (buffer[0] == KERN_SOH_ASCII && buffer[1]) {
+    switch (buffer[1]) {
+    case '0' ... '7':
+    case 'd':	/* KERN_DEFAULT */
+      return buffer[1];
+    }
+  }
 #else
-        if (buffer[0] == '<' && buffer[1] && buffer[2] && buffer[2] == '>') {
-	  switch(buffer[1]) {
-	  case '0' ... '7':
-	  case 'd':
-	    return buffer[1];
-	      }
-	}
+  if (buffer[0] == '<' && buffer[1] && buffer[2] && buffer[2] == '>') {
+    switch(buffer[1]) {
+    case '0' ... '7':
+    case 'd':    
+      return buffer[1];
+    }
+  }
 #endif
-	return 0;
+  return 0;
 }
 static inline const char *rpk_skip_level(const char *buffer)
 {
   int header_len = 0;
-#ifdef KERN_SOH_ASCII // New kernel style
+#ifdef KERN_SOH_ASCII // New kernel style - 3.x
   header_len = 2;
 #else
   header_len = 3;
 #endif
-	if (rpk_get_level(buffer)) {
-		switch (buffer[1]) {
-		case '0' ... '7':
-		case 'd':	/* KERN_DEFAULT */
-			return buffer + header_len;
-		}
-	}
-	return buffer;
+  if (rpk_get_level(buffer)) {
+    switch (buffer[1]) {
+    case '0' ... '7':
+    case 'd':	/* KERN_DEFAULT */
+      return buffer + header_len;
+    }
+  }
+  return buffer;
 }
 
 static int rpk_encode2(char *buf, size_t size, 
-			  const char *fmt, va_list args)
+		       const char *fmt, va_list args)
 {
   int kern_level = 0;
   char rpk_level[3];
@@ -308,6 +322,7 @@ static int rpk_encode2(char *buf, size_t size,
   if (end < buf) 
     end = (void *)-1;
 
+  /* Get kern level if part of string */
   kern_level = rpk_get_level(fmt);
   if (kern_level) {
     rpk_level[0] = '<', rpk_level[1] = fmt[1], rpk_level[2] = '>';
@@ -326,16 +341,16 @@ static int rpk_encode2(char *buf, size_t size,
 
 
 static struct jprobe my_jprobe = {
-	.kp.addr = (kprobe_opcode_t *) printk,
-	.entry = (kprobe_opcode_t *) ins_printk
+  .kp.addr = (kprobe_opcode_t *) printk,
+  .entry = (kprobe_opcode_t *) ins_printk
 };
 
 int init_module(void)
 {
-	register_jprobe(&my_jprobe);
-	printk(KERN_INFO "plant printk jprobe at %p, handler addr %p",
-	       my_jprobe.kp.addr, my_jprobe.entry);
-	return 0;
+  register_jprobe(&my_jprobe);
+  printk(KERN_INFO "plant printk jprobe at %p, handler addr %p",
+	 my_jprobe.kp.addr, my_jprobe.entry);
+  return 0;
 }
 
 void cleanup_module(void)
--- test_module/printk_testing.c
+++ test_module/printk_testing.c
@@ -2,12 +2,23 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+void test_pV(const char *fmt, ...) 
+{
+  va_list args;
+  struct va_format vaf;
+  va_start(args,fmt);
+  vaf.fmt = fmt;
+  vaf.va = &args;
+  printk(KERN_INFO "trying out pV: %pV",&vaf);
+}
+
 int hello_init(void) {
   int a = 0;
   char *b = "looks-good";
   
   printk(KERN_INFO "format string for data: %d %s",a,b);
-
+  test_pV("%s %d","string" "-test",111);
+  
   return 0;
 
 }
